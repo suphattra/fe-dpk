977c70a223fdcd664dfeb64208b12d6bb4e8a4b2
suphs443 <suphs443@entro.postbox.in.th>
2024-01-15 00:41:30 +0700

    fix: cosmetic branch

--- components/branches/ModalUpdateBranch.js
+++ components/branches/ModalUpdateBranch.js
@@ -162,48 +162,44 @@ export default function ModalUpdateBranch(props) {
         <Transition.Root show={open} as={Fragment}>
             <Dialog as="div" className="relative z-10" onClose={setOpen}>
                 <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+                >
+                <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                </Transition.Child>
+                <div className="fixed inset-0 z-10 overflow-y-auto p-14">
+                <div className="flex max-h-fit min-h-max h-full items-endjustify-center p-4 text-center sm:items-center sm:p-0 ">
+                    <Transition.Child
                     as={Fragment}
                     enter="ease-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
+                    enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    enterTo="opacity-100 translate-y-0 sm:scale-100"
                     leave="ease-in duration-200"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-                </Transition.Child>
+                    leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                    leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  
+                    >
+                           <Dialog.Panel className="h-full relative transform  rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full">
+                            <div className="h-5/6 overflow-y-auto shadow-inner border rounded-md">
+                                    {querySuccess &&
+                                        <CardBranch
+                                            index={1}
+                                            branch={branchDetail}
+                                            onChange={onChange}
+                                            mode={mode}
+                                            // deleteAddOnService={deleteAddOnService}
+                                            onErrors={errors}
+                                        />
+                                    }
+                                </div>
+                            <footer className="flex items-center justify-center">
 
-                <div className="fixed inset-0 z-10 overflow-y-auto w-100">
-                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                        <Transition.Child
-                            as={Fragment}
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                            enterTo="opacity-100 translate-y-0 sm:scale-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                        >
-                            <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 text-left shadow-xl transition-all w-5/6 h-3/6 md:h-auto p-6">
-                                {/* <div className="rounded-md p-4 shadow-md"> */}
-                                {querySuccess &&
-                                    // <div className="flex flex-1 items-stretch">
-                                    <CardBranch
-                                        index={1}
-                                        branch={branchDetail}
-                                        onChange={onChange}
-                                        mode={mode}
-                                        // deleteAddOnService={deleteAddOnService}
-                                        onErrors={errors}
-                                    />
-                                    // </div>
-                                }
-                                {/* </div> */}
-
-                                {/* <CardTimesheet index={0} timeSheet={timesheetDetail} onChange={onChange} deleteAddOnService={deleteAddOnService} mode={mode} /> */}
-                                < footer className="flex items-center justify-center sm:px-6 lg:px-8 sm:py-4 lg:py-4">
-
-                                    <div className="flex justify-center items-center overflow-y-auto p-4" >
+                                    <div className="flex justify-center items-center overflow-y-auto my-10" >
                                         <div className="flex justify-center items-center">
                                             <button type="button"
                                                 className="flex justify-center inline-flex items-center rounded-md border border-transparent bg-gray-600 px-6 py-1 pb-1.5 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 mr-2"
@@ -221,7 +217,7 @@ export default function ModalUpdateBranch(props) {
                                         </div>
                                     </div>
 
-                                </footer>
+                            </footer>
 
                             </Dialog.Panel>
                         </Transition.Child>
