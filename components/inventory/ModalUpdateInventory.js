d5e737e1413d6c3ac7b4a54193ffe912cdd684f2
suphs443 <suphs443@entro.postbox.in.th>
2024-01-14 23:05:06 +0700

    fix: cosmetic modal

--- components/inventory/ModalUpdateInventory.js
+++ components/inventory/ModalUpdateInventory.js
@@ -171,9 +171,8 @@ export default function ModalUpdateInventory(props) {
         >
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
-
-        <div className="fixed inset-0 z-10">
-          <div className="flex items-end justify-center text-center sm:items-center sm:p-0">
+        <div className="fixed inset-0 z-10 overflow-y-auto p-14">
+          <div className="flex max-h-fit min-h-max h-full items-endjustify-center p-4 text-center sm:items-center sm:p-0 ">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -182,10 +181,14 @@ export default function ModalUpdateInventory(props) {
               leave="ease-in duration-200"
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              // className="overflow-auto "
+              // leaveFrom="translate-x-0"
+              // leaveTo="-translate-x-full"
             >
-              <Dialog.Panel className="relative transform  rounded-lg bg-white  text-left shadow-xl transition-all w-5/6 ">
-                {querySuccess && (
-                  <div className=" md:h-auto max-h-screen overflow-y-auto">
+              
+              <Dialog.Panel className="h-full relative transform  rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full">
+              <div className="h-5/6 overflow-y-auto shadow-inner">
+             {querySuccess && (
                     <CardInventory
                       index={1}
                       inventory={inventoryDetail}
@@ -194,32 +197,32 @@ export default function ModalUpdateInventory(props) {
                       // deleteAddOnService={deleteAddOnService}
                       onErrors={errors}
                     />
-                    <footer className="flex items-center justify-center sm:px-6 lg:px-8 sm:py-4 lg:py-4">
-                      <div className="flex justify-center items-center overflow-y-auto p-4">
-                        <div className="flex justify-center items-center">
+                  )}
+                   </div>
+                  <footer className="flex  h-1/6 items-center justify-center ">
+                    <div className="flex justify-center items-center overflow-y-auto ">
+                      <div className="flex justify-center items-center">
+                        <button
+                          type="button"
+                          className="flex justify-center inline-flex items-center rounded-md border border-transparent bg-gray-600 px-6 py-1 pb-1.5 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 mr-2"
+                          onClick={() => {
+                            setOpen(false);
+                          }}
+                        >
+                          {mode === "edit" ? "ยกเลิก" : "ปิดหน้าต่าง"}
+                        </button>
+                        {mode === "edit" && (
                           <button
                             type="button"
-                            className="flex justify-center inline-flex items-center rounded-md border border-transparent bg-gray-600 px-6 py-1 pb-1.5 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 mr-2"
-                            onClick={() => {
-                              setOpen(false);
-                            }}
+                            className="flex justify-center inline-flex items-center rounded-md border border-transparent bg-purple-600 px-6 py-1 pb-1.5 text-sm font-medium text-white shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                            onClick={handleSave}
                           >
-                            {mode === "edit" ? "ยกเลิก" : "ปิดหน้าต่าง"}
+                            บันทึก
                           </button>
-                          {mode === "edit" && (
-                            <button
-                              type="button"
-                              className="flex justify-center inline-flex items-center rounded-md border border-transparent bg-purple-600 px-6 py-1 pb-1.5 text-sm font-medium text-white shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                              onClick={handleSave}
-                            >
-                              บันทึก
-                            </button>
-                          )}
-                        </div>
+                        )}
                       </div>
-                    </footer>
-                  </div>
-                )}
+                    </div>
+                  </footer>
               </Dialog.Panel>
             </Transition.Child>
           </div>
